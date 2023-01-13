5064d49a18abe80e8d0ea8352e20eca50a07ee0b
Henrique Estrela <henrique.estrela50@hotmail.com>
2023-01-13 17:45:46 -0300

    imports

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,16 @@
-import Header from '../components/Header'
+import Banner from '../components/Banner';
+import Header from '../components/Header';
 import Logo from '../components/Logo';
+import Main from '../components/Main';
 
 function Home(){
     return(
        <>
             <Logo/>
             <Header/>
+            <Banner/>
+            <Main/>
+            
        </>
     )
 }
