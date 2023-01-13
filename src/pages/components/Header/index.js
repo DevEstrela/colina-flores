8c266153db9d9c54d322a93c7986f2bdcb6ac6b7
Henrique Estrela <henrique.estrela50@hotmail.com>
2023-01-13 10:06:35 -0300

    edit-header

--- src/pages/components/Header/index.js
+++ src/pages/components/Header/index.js
@@ -1,12 +1,24 @@
 import styles from '../Header/Header.module.css';
-import logo from '../../../img/logo.png';
+import { Link } from 'react-router-dom';
 
 function Header(){
     return(
         <header className={styles.container}>
-            <div className={styles.containerLogo}>
-                <img alt='logo' src={logo} className={styles.logo}/>
-            </div>
+            
+            <nav className={styles.containerNav}>
+                    <h1>Seu Carinho com Flores</h1> 
+                
+                    <Link className={styles.ContainerLinks}>
+                        <a className={styles.links} href='#'>Localização</a>
+                        <a className={styles.links} href='#'>Contatos</a>
+                        <a className={styles.links} href='#'>Coroas</a>
+
+                        <a href='#zap' className={styles.button}>Entrar em contato</a>
+
+                    </Link>
+                    
+            </nav>
+
         </header>
     )
 }
