a1f549498df65582fdc8d8cc9daba25f4ad54417
Henrique Estrela <henrique.estrela50@hotmail.com>
2023-01-13 17:44:39 -0300

    import img

--- src/pages/components/Banner/index.js
+++ src/pages/components/Banner/index.js
@@ -1,8 +1,9 @@
 import styles from '../Banner/Banner.module.css'
-
+import banner from '../../../img/banner.png'
 function Banner(){
     return(
-        
+        <section className={styles.containerBanner}>
+        </section>
     )
 }
 export default Banner;
