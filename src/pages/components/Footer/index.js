54546b221492a5e76a70615ff412b5dd019535b5
Henrique Estrela <henrique.estrela50@hotmail.com>
2023-01-19 12:02:58 -0300

    create Footer

---
+++ src/pages/components/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from '../Footer/Footer.module.css';
+
+function Footer(){
+    return(
+        <footer>
+            <h1>HENRIQUUUUUUUUE</h1>
+        </footer>
+    )
+}
+export default Footer;
