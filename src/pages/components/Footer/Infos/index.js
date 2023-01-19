587346ca2a6e7b2449b8a1fcdd97251598fc957a
Henrique Estrela <henrique.estrela50@hotmail.com>
2023-01-19 16:50:41 -0300

    import

--- src/pages/components/Footer/Infos/index.js
+++ src/pages/components/Footer/Infos/index.js
@@ -1,34 +1,61 @@
 import styles from '../Infos/Infos.module.css';
 import boton from '../../../../img/footer/boton.png'
-import face from '../../../../img/footer/face.png';
-import insta from '../../../../img/footer/insta.png';
-import linke from '../../../../img/footer/linke.png';
+import local from '../../../../img/footer/local.png';
+import wpp from '../../../../img/footer/wpp.png';
+import email from '../../../../img/footer/email.png';
 import send from '../../../../img/footer/send.png'
+import { Link } from 'react-router-dom';
 
 
 function Infos(){
     return(
         <section className={styles.container}>
 
-            <nav>
-                <h4>Paginas</h4>
-                <img src={send} /> <p>Inicio</p>
-                <img src={send} /> <p>Blog</p>
-                <img src={send} /> <p>Trabalhe Conosco</p>
-            </nav>
-
-            <nav>
-                <h4>Serviços</h4>
-                <img src={send} /> <p>Funerario</p>
-                <img src={send} /> <p>Cremação</p>
-                <img src={send} /> <p>Planos</p>
-            </nav>
-
-            <nav>
+<nav className={styles.containerNav}>
                 <h4>Contatos</h4>
-                <img src={send} /> <p>(11) 4746-7777</p>
-                <img src={send} /> <p>colinadosipes@gmail.com</p>
-                <img src={send} /> <p>(11) 9 58584-747</p>
+                    <div>
+                        <img src={send} /> <Link to="/" className={styles.links}>Inicio</Link>
+                    </div>
+                    
+                    <div>
+                        <img src={send} /> <Link to="/" className={styles.links}>Blog</Link>
+                    </div>
+                    
+                    <div>
+                        <img src={send} /> <Link to="/" className={styles.links}>Trabale Conosco</Link>
+                    </div>
+                                    
+            </nav>
+            <nav className={styles.containerNav}>
+                <h4>Contatos</h4>
+                    <div>
+                        <img src={send} /> <Link to="/" className={styles.links}>Funerário</Link>
+                    </div>
+                    
+                    <div>
+                        <img src={send} /> <Link to="/" className={styles.links}>Cremação</Link>
+                    </div>
+                    
+                    <div>
+                        <img src={send} /> <Link to="/" className={styles.links}>Linklanos</Link>
+                    </div>
+                                    
+            </nav>
+
+            <nav className={styles.containerNav}>
+                <h4>Contatos</h4>
+                    <div>
+                        <img src={local} /> <p>(11) 4746-7777</p>
+                    </div>
+                    
+                    <div>
+                        <img src={email} /> <p>colinadosipes@gmail.com</p>
+                    </div>
+                    
+                    <div>
+                        <img src={wpp} /> <p>(11) 9 58584-747</p>
+                    </div>
+                                    
             </nav>
 
             <div>
