66d0a8d256d9aa9de31d2639b37e192a6ffab481
Henrique Estrela <henrique.estrela50@hotmail.com>
2023-01-27 08:42:04 -0300

    Delete-banner

--- src/pages/components/Banner/index.js
+++
@@ -1,8 +0,0 @@
-import styles from '../Banner/Banner.module.css'
-function Banner(){
-    return(
-        <section id='topo' className={styles.containerBanner}>
-        </section>
-    )
-}
-export default Banner;
